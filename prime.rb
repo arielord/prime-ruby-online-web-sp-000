a3dba7dc10d0c121d699ff959fd5dca5776f3215
Ariel <arielord@buffalo.edu>
2018-12-13 04:34:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime(num)
+  if num == 0
+    return false
+  elsif num == 1
+    return true
+  else
+    i = 2
+    while i < num do
+      num % i == 0 ? false : true
+      i += 1
+    end
+  end
+end
